e7abb957a682d7ba58f6bc72f9bfb901aaa952e9
javosanc <javieromarsanchezosorio@gmail.com>
2017-05-04 18:25:05 -0500

    added more functionality

--- utilities.js
+++ utilities.js
@@ -1,41 +1,52 @@
-//retorna objeto con los tiempos y los valores de los markers
-function getMarkerValues(layer){
-	var markers = {
-		times:[],
-		values:[]
-	};
+var AEUtils = (function (){
+	//retorna objeto con los tiempos y los valores de los markers
+	function getMarkerValues(layer){
+		var markers = {
+			times:[],
+			values:[]
+		};
 
-	for (var i = 0; i < layer.marker.numKeys; i++) {
-		markers.times.push(layer.marker.keyTime(i+1));
-		markers.values.push(layer.marker.keyValue(i+1).comment);
+		for (var i = 0; i < layer.marker.numKeys; i++) {
+			markers.times.push(layer.marker.keyTime(i+1));
+			markers.values.push(layer.marker.keyValue(i+1).comment);
+		}
+
+		return markers;
 	}
 
-	return markers;
-}
+	function assignKeyframes(property, markers, values){
 
-function assignKeyframes(property, markers, values){
-
-	property.setValuesAtTimes(markers.times, values)
-}
-
-function deleteAllKeys(property){
-	while(property.numKeys > 0){
-		property.removeKey(1);
+		property.setValuesAtTimes(markers.times, values)
 	}
-}
 
-function deleteKeysBetween(property, startTime, endTime){
-	for(var i = property.numKeys; i > 0; i--){
-		if(property.keyTime(i) >= startTime && property.keyTime(i) <= endTime){
-			property.removeKey(i);
+	function deleteAllKeys(property){
+		while(property.numKeys > 0){
+			property.removeKey(1);
 		}
 	}
-}
 
-function scaleKeys(times, values, scale, offset){
-	for (var i = 0; i < times.length; i++){
-    	times[i] = times[i] + comp.time;
-    	values[i] = values[i] * scale + offset;
+	function deleteKeysBetween(property, startTime, endTime){
+		for (var i = property.numKeys; i > 0; i--){
+			if(property.keyTime(i) >= startTime && property.keyTime(i) <= endTime){
+				property.removeKey(i);
+			}
+		}
 	}
 
-}
+	function scaleKeys(timesArray, valuesArray, scale, timeOffset, valueOffset){
+		for (var i = 0; i < timesArray.length; i++){
+	    	timesArray[i] = timesArray[i] + timeOffset;
+	    	valuesArray[i] = valuesArray[i] * scale + valueOffset;
+		}
+
+	}
+
+	return {
+		getMarkerValues: getMarkerValues,
+		assignKeyframes: assignKeyframes,
+		deleteAllKeys: deleteAllKeys,
+		deleteKeysBetween: deleteKeysBetween,
+		scaleKeys: scaleKeys
+	}
+
+})();
